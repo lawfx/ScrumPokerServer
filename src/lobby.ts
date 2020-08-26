f00aebb48a7c591519e3eeb494c80fa6bc5777da
Nikolaos Ioannou <ioannounikos93@gmail.com>
2020-08-26 20:42:19 +0200

    handling the case where the name query is undefined

--- src/lobby.ts
+++ src/lobby.ts
@@ -282,7 +282,7 @@ export class Lobby {
     for (let i = 0; i < vars.length; i++) {
       const pair = vars[i].split('=');
       if (pair[0] == variable) {
-        return pair[1].trim();
+        return pair[1]?.trim();
       }
     }
   }
